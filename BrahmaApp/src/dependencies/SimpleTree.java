744d0b28b14fede10455a53f9c581fac418e0d77
collinbc <collinbc@rose-hulman.edu>
2012-09-27 00:30:22 -0400

    Iterator is built as an internal class within SimpleTree. I haven't tested it yet.

--- BrahmaApp/src/dependencies/SimpleTree.java
+++ BrahmaApp/src/dependencies/SimpleTree.java
@@ -3,7 +3,6 @@ package dependencies;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import dependencies.exception.CycleException;
 
@@ -59,36 +58,6 @@ public class SimpleTree<T> {
 		return data;
 	}
 	
-	public Iterator<T> getChildrenFirstIterator() {
-		return new ChildrenFirstIterator(this);
-	}
-	
-	private class ChildrenFirstIterator implements Iterator<T> {
-		SimpleTree<T> tree;
-		
-		public ChildrenFirstIterator(SimpleTree<T> tree) {
-			this.tree = tree;
-		}
-
-		@Override
-		public boolean hasNext() {
-			// TODO: implement
-			return false;
-		}
-
-		@Override
-		public T next() {
-			// TODO: implement
-			return null;
-		}
-
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-			
-		}
-	}
-	
 	private boolean checkHasAncestor(SimpleTree<T> parent) {
 		SimpleTree<T> currentNode = this;
 		do {
@@ -99,5 +68,69 @@ public class SimpleTree<T> {
 		}
 		while(currentNode != null);
 		return false;
+	}
+	
+	public Iterator<T> getChildrenFirstIterator() {
+		return new ChildrenFirstIterator(this);
+	}
+	
+	private class ChildrenFirstIterator implements Iterator<T> {
+		private final SimpleTree<T> root;
+		private SimpleTree<T> node;
+		private int pos = 0;
+		
+		public ChildrenFirstIterator(SimpleTree<T> root) {
+			this.root = root;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return (!this.node.equals(this.root));
+		}
+
+		@Override
+		public T next() {
+			if (this.node == null)
+				setToFirst();
+			else
+				next(this.node);
+			return this.node.data;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+		
+		private void next(SimpleTree<T> previous) {
+			if (!hasNext()) {
+				setToFirst();
+			}
+			
+			if (this.pos < 0) {
+				this.node = root;
+			}
+			
+			SimpleTree<T> parent = previous.parent;
+			this.pos++;
+			
+			if (parent.children.size() <= this.pos) {
+				this.pos = getIndex(parent);
+				next(parent);
+			}
+			this.node = (SimpleTree<T>) parent.children.get(this.pos);
+		}
+
+		private void setToFirst() {
+			this.node = this.root;
+			while(this.node.children.size() > 0) {
+				this.node = (SimpleTree<T>) this.node.children.get(0);
+			}
+		}
+		
+		private int getIndex(SimpleTree<T> node) {
+			if(node.parent == null) return -1;
+			return node.parent.children.indexOf(this);
+		}
 	}
 }
