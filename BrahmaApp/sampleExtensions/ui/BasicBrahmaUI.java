046e474ad70652f1b89e8a2d3d335bfdada4d52b
collinbc <collinbc@rose-hulman.edu>
2012-10-03 11:11:48 -0400

    UI fixed

--- BrahmaApp/sampleExtensions/ui/BasicBrahmaUI.java
+++ BrahmaApp/sampleExtensions/ui/BasicBrahmaUI.java
@@ -5,26 +5,26 @@ import java.util.List;
 
 import javax.swing.JFrame;
 
-import plugin.IPlugin;
+import plugin.ILoadableApplication;
 import extension.IPluginListenerExtension;
 
 public class BasicBrahmaUI implements IPluginListenerExtension {
 	private JFrame frame;
-	private List<IPlugin> plugins;
+	private List<ILoadableApplication> plugins;
 	
 	public BasicBrahmaUI() {
-		this.plugins = new ArrayList<IPlugin>();
+		this.plugins = new ArrayList<ILoadableApplication>();
 		// TODO: Make a frame class. this.frame = ?;
 	}
 	
 	@Override
-	public void pluginLoaded(IPlugin plugin) {
+	public void pluginLoaded(ILoadableApplication plugin) {
 		this.plugins.add(plugin);
 		// TODO: Ad to frame
 	}
 
 	@Override
-	public void pluginUnloaded(IPlugin plugin) {
+	public void pluginUnloaded(ILoadableApplication plugin) {
 		this.plugins.remove(plugin);
 		// TODO: Remove from frame
 	}
